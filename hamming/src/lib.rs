9d6de31cffe256f1374d59d084bd890f790eb5f7
John Davies <jd.other@hotmail.com>
2024-09-29 23:09:28 +0700

    wrapped final expression in Some(), implicit return

--- hamming/src/lib.rs
+++ hamming/src/lib.rs
@@ -1,15 +1,13 @@
 pub fn hamming_distance(s1: &str, s2: &str) -> Option<usize> {
-    // collect into an Option::<usize>
-    // try and return this implicitly and use Option<usize>
     if s1.len() != s2.len() {
         return None;
     }
 
-    let distance: usize = s1
-        .char_indices()
-        .fold(0, |distance: usize, (i, c)| match c {
-            c if c != s2.chars().nth(i).unwrap() => distance + 1,
-            _ => distance,
-        });
-    Some(distance)
+    Some(
+        s1.char_indices()
+            .fold(0, |distance: usize, (i, c)| match c {
+                c if c != s2.chars().nth(i).unwrap() => distance + 1,
+                _ => distance,
+            }),
+    )
 }
