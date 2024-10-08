84223ceb38bf897c332bcf5ee2455904509bae6e
John Davies <jd.other@hotmail.com>
2024-10-09 01:31:26 +0700

    implemented solution for scrabble score

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # exercism-solutions-rust
 Solutions to the [Exercism](https://exercism.org/tracks/rust) exercises for the Rust track\
-26/98 completed so far
+27/98 completed so far
---
+++ scrabble-score/src/lib.rs
@@ -0,0 +1,18 @@
+/// Compute the Scrabble score for a word.
+pub fn score(word: &str) -> u64 {
+    word.chars()
+        .map(|mut c| {
+            c = c.to_ascii_uppercase();
+            match c {
+                'A' | 'E' | 'I' | 'O' | 'U' | 'L' | 'N' | 'R' | 'S' | 'T' => 1,
+                'D' | 'G' => 2,
+                'B' | 'C' | 'M' | 'P' => 3,
+                'F' | 'H' | 'V' | 'W' | 'Y' => 4,
+                'K' => 5,
+                'J' | 'X' => 8,
+                'Q' | 'Z' => 10,
+                _ => 0,
+            }
+        })
+        .sum::<u64>()
+}
